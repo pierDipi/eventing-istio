cf2bfebe17252d1b93fb593818ad43f914718b68
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-03-30 11:11:46 +0200

    Point GOPATH to writable directory
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- openshift/e2e-tests.sh
+++ openshift/e2e-tests.sh
@@ -3,4 +3,6 @@
 repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
 
 export SKIP_INITIALIZE=true
+export GOPATH=/tmp/go
+export GOCACHE=/tmp/go-cache
 "${repo_root_dir}/test/e2e-tests.sh"
