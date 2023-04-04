8c9b18c4d0e76cd0e512ff74c52aeb67fbebed83
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-04-04 11:54:04 +0200

    Point to openshift artifact directory
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- openshift/e2e-tests.sh
+++ openshift/e2e-tests.sh
@@ -5,4 +5,6 @@ repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
 export SKIP_INITIALIZE=true
 export GOPATH=/tmp/go
 export GOCACHE=/tmp/go-cache
+export ARTIFACTS=${ARTIFACT_DIR:-$(mktemp -u -t -d)}
+
 "${repo_root_dir}/test/e2e-tests.sh"
