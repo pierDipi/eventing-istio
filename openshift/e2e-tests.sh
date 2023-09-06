ac78cfb8d5b9c43845f0009993a10b6d3e6ceaf7
Christoph Stäbler <cstabler@redhat.com>
2023-09-06 12:35:05 +0200

    Remove submodule patch to skip pingsource event type test (#62)
    Revert "Apply submodule patch to skip EventType tests"
    This reverts commit 4db20aa32ee43610928a989682bd387b3cd7834f.

--- openshift/e2e-tests.sh
+++ openshift/e2e-tests.sh
@@ -7,9 +7,4 @@ export GOPATH=/tmp/go
 export GOCACHE=/tmp/go-cache
 export ARTIFACTS=${ARTIFACT_DIR:-$(mktemp -u -t -d)}
 
-pushd "${repo_root_dir}/third_party/eventing"
-echo "Apply eventing submodule patches"
-git apply -v ../../openshift/submodule-patches/eventing/*
-popd
-
 "${repo_root_dir}/test/e2e-tests.sh"
