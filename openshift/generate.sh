c911d5e2b2d43caf25129df570bc8e9ceb338ebb
Christoph Stäbler <cstabler@redhat.com>
2024-06-27 11:18:27 +0200

    Clone hack repo to install generate (#267)

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -31,7 +31,14 @@ function resolve_resources(){
 
 "${repo_root_dir}/hack/update-deps.sh"
 
-GO111MODULE=off go get -u github.com/openshift-knative/hack/cmd/generate
+tmp_dir=$(mktemp -d)
+git clone --branch main https://github.com/openshift-knative/hack "$tmp_dir"
+
+pushd "$tmp_dir"
+go install github.com/openshift-knative/hack/cmd/generate
+popd
+
+rm -rf "$tmp_dir"
 
 $(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
