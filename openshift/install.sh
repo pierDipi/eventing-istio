78552fb07f0ae7eb56bc51f8de629db559fe1e56
Pierangelo Di Pilato <pierdipi@redhat.com>
2024-09-02 11:04:21 +0200

    Fix e2e tests
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- openshift/install.sh
+++ openshift/install.sh
@@ -7,12 +7,17 @@ function install_eventing_with_mesh() {
     KNATIVE_EVENTING_ISTIO_MANIFESTS_DIR="${SCRIPT_DIR}/release/artifacts"
     export KNATIVE_EVENTING_ISTIO_MANIFESTS_DIR
 
-    GO111MODULE=off go get -u github.com/openshift-knative/hack/cmd/sobranch
+    GO111MODULE=off go install github.com/openshift-knative/hack/cmd/sobranch
 
     local release
     release=$(yq r "${SCRIPT_DIR}/project.yaml" project.tag)
     release=${release/knative-/}
     so_branch=$( $(go env GOPATH)/bin/sobranch --upstream-version "${release}")
+
+    USE_IMAGE_RELEASE_TAG="$(yq r "${SCRIPT_DIR}/project.yaml" project.tag)"
+    export USE_IMAGE_RELEASE_TAG
+
+    echo "Tag: ${USE_IMAGE_RELEASE_TAG}"
 
     local operator_dir=/tmp/serverless-operator
     git clone --branch "${so_branch}" https://github.com/openshift-knative/serverless-operator.git $operator_dir || git clone --branch main https://github.com/openshift-knative/serverless-operator.git $operator_dir
