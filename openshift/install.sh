36d3e38220eb4d9be4bf2ec6c808b923e5a2756b
Pierangelo Di Pilato <pierdipi@redhat.com>
2024-09-26 11:59:11 +0200

    [release-v1.15] Fix e2e tests (#289)
    * Fix e2e tests
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Generate SO files
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Set GOPATH to writable directory
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Set GOMODCACHE
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Upgrade submodules
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    ---------
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- openshift/install.sh
+++ openshift/install.sh
@@ -4,15 +4,23 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 function install_eventing_with_mesh() {
 
+    export GOPATH=/tmp/go
+    export GOMODCACHE=/tmp/go/modcache
+
     KNATIVE_EVENTING_ISTIO_MANIFESTS_DIR="${SCRIPT_DIR}/release/artifacts"
     export KNATIVE_EVENTING_ISTIO_MANIFESTS_DIR
 
-    GO111MODULE=off go get -u github.com/openshift-knative/hack/cmd/sobranch
+    GO111MODULE=off go install github.com/openshift-knative/hack/cmd/sobranch
 
     local release
     release=$(yq r "${SCRIPT_DIR}/project.yaml" project.tag)
     release=${release/knative-/}
     so_branch=$( $(go env GOPATH)/bin/sobranch --upstream-version "${release}")
+
+    USE_IMAGE_RELEASE_TAG="$(yq r "${SCRIPT_DIR}/project.yaml" project.tag)"
+    export USE_IMAGE_RELEASE_TAG
+
+    echo "Tag: ${USE_IMAGE_RELEASE_TAG}"
 
     local operator_dir=/tmp/serverless-operator
     git clone --branch "${so_branch}" https://github.com/openshift-knative/serverless-operator.git $operator_dir || git clone --branch main https://github.com/openshift-knative/serverless-operator.git $operator_dir
@@ -22,7 +30,7 @@ function install_eventing_with_mesh() {
     export ON_CLUSTER_BUILDS=true
     export DOCKER_REPO_OVERRIDE=image-registry.openshift-image-registry.svc:5000/openshift-marketplace
 
-    make OPENSHIFT_CI="true" SCALE_UP=5 TRACING_BACKEND=zipkin images install-certmanager install-strimzi install-kafka-with-mesh || return $?
+    make OPENSHIFT_CI="true" SCALE_UP=5 TRACING_BACKEND=zipkin generated-files images install-certmanager install-strimzi install-kafka-with-mesh || return $?
 
     popd || return $?
 }
